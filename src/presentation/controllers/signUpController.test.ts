c0c3a9b860b0c0d3f82a4542eec0e067ebdcf70b
Felipe Mendonca <felipemom2001@gmail.com>
2022-08-18 17:44:05 +0200

    refactor: add types to HttpRequest and HttpResponse

--- src/presentation/controllers/signUpController.test.ts
+++ src/presentation/controllers/signUpController.test.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
 class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
@@ -13,6 +15,8 @@ class SignUpController {
         body: new Error('Missing param: Email'),
       };
     }
+
+    return { statusCode: 200, body: {} };
   }
 }
 
