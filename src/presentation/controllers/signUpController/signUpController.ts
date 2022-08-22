9f8626ed0f4d03a0db618fad4a236f647d159814
Felipe Mendonca <felipemom2001@gmail.com>
2022-08-22 18:56:26 +0200

    refactor: simplify SignUpController variables names

--- src/presentation/controllers/signUpController/signUpController.ts
+++ src/presentation/controllers/signUpController/signUpController.ts
@@ -15,18 +15,21 @@ export class SignUpController implements Controller {
       'passwordConfirmation',
     ];
 
+    //Ensure all required fields above are provided
     for (const field of requiredFields) {
       if (!httpRequest.body[field])
         return badRequest(new MissingParamError(field));
     }
 
+    const { email, password, passwordConfirmation } = httpRequest.body;
+
     try {
-      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email);
+      const emailIsValid = this.emailValidator.isValid(email);
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'));
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'));
       }
 
