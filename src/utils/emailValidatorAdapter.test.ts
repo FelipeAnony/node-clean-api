532da178ed87e9adc1d0b7b6ac4e21e196c26e85
Felipe Mendonca <felipemom2001@gmail.com>
2022-08-25 20:25:02 +0200

    chore: add email validator

--- src/utils/emailValidatorAdapter.test.ts
+++ src/utils/emailValidatorAdapter.test.ts
@@ -12,4 +12,10 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalid_email@gmail.com');
     expect(isValid).toBe(false);
   });
+
+  // it('Should return true if validator returns true', () => {
+  //   const sut = new EmailValidatorAdapter();
+  //   const isValid = sut.isValid('invalid_email@gmail.com');
+  //   expect(isValid).toBe(true);
+  // });
 });
