3b8131fe1acd719d171555dbe33adaaeb2ad89c6
Felipe Mendonca <felipemom2001@gmail.com>
2022-08-25 20:38:08 +0200

    feat: ensure that EmailValidator.isValid returns true if validator returns true

--- src/utils/emailValidatorAdapter.test.ts
+++ src/utils/emailValidatorAdapter.test.ts
@@ -1,21 +1,28 @@
 import { EmailValidator } from '@/presentation/controllers/signUpController/signUpProtocols';
+import validator from 'validator';
+
+jest.mock('validator');
+
+const mockedIsEmail = jest.spyOn(validator, 'isEmail');
+mockedIsEmail.mockReturnValue(true);
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
-    return false;
+    return validator.isEmail(email);
   }
 }
 
 describe('EmailValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
+    mockedIsEmail.mockReturnValueOnce(false);
     const sut = new EmailValidatorAdapter();
-    const isValid = sut.isValid('invalid_email@gmail.com');
+    const isValid = sut.isValid('invalid-email');
     expect(isValid).toBe(false);
   });
 
-  // it('Should return true if validator returns true', () => {
-  //   const sut = new EmailValidatorAdapter();
-  //   const isValid = sut.isValid('invalid_email@gmail.com');
-  //   expect(isValid).toBe(true);
-  // });
+  it('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('valid@email');
+    expect(isValid).toBe(true);
+  });
 });
