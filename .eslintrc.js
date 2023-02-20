94b1f475cc9af26f23e1e2d2fb8299850bdf4480
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-02-20 21:14:08 +0100

    chore: configs eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  env: {
+    node: true,
+  },
+};
