10bdea49986be9f10d31cd1aa9e59fbcf88bcaa2
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-02-24 20:55:59 +0100

    feat: ensures signup controller returns 400 if no name is provided

---
+++ src/presentation/controllers/signup-controller.test.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker';
+
+import { SignUpController } from './signup-controller';
+
+describe('SignUp controller', () => {
+    it('Should return 400 if theres no name provided', () => {
+        const sut = new SignUpController();
+        const password = faker.internet.password(32);
+
+        const httpRequest = {
+            email: faker.internet.email(),
+            password,
+            passwordConfirmation: password,
+        };
+
+        const response = sut.handle(httpRequest);
+
+        expect(response.statusCode).toBe(400);
+    });
+});
