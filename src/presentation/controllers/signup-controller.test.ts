b6e5312f4278c8f88a2c1019bc43182e35f389ac
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-02-27 20:30:59 +0100

    feat: ensures signup controller returns 400 and the correct error message if theres no email provided

--- src/presentation/controllers/signup-controller.test.ts
+++ src/presentation/controllers/signup-controller.test.ts
@@ -8,14 +8,34 @@ describe('SignUp controller', () => {
         const password = faker.internet.password(32);
 
         const httpRequest = {
-            email: faker.internet.email(),
-            password,
-            passwordConfirmation: password,
+            body: {
+                email: faker.internet.email(),
+                password,
+                passwordConfirmation: password,
+            },
         };
 
         const response = sut.handle(httpRequest);
 
         expect(response.statusCode).toBe(400);
         expect(response.body).toEqual(new Error('Missing param: name'));
+    });
+
+    it('Should return 400 if theres no email provided', () => {
+        const sut = new SignUpController();
+        const password = faker.internet.password(32);
+
+        const httpRequest = {
+            body: {
+                name: faker.name.firstName(),
+                password,
+                passwordConfirmation: password,
+            },
+        };
+
+        const response = sut.handle(httpRequest);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toEqual(new Error('Missing param: email'));
     });
 });
