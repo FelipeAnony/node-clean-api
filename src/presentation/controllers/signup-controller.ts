4ec5834bde013b83a2485c211b91d5d68147aa20
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-03-03 22:55:20 +0100

    refactors: moves all code in sign-up-controller to try-catch statement

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -6,13 +6,13 @@ export class SignUpController implements Controller {
     constructor(private readonly emailValidator: EmailValidator) {}
 
     handle({ body }: HttpRequest): HttpResponse {
-        const requiredParams = ['name', 'email', 'password', 'passwordConfirmation'];
-
-        for (const field of requiredParams) {
-            if (!body[field]) return badRequest(new MissingParamError(field));
-        }
-
         try {
+            const requiredParams = ['name', 'email', 'password', 'passwordConfirmation'];
+
+            for (const field of requiredParams) {
+                if (!body[field]) return badRequest(new MissingParamError(field));
+            }
+
             const emailIsValid = this.emailValidator.isValid(body.email);
             if (!emailIsValid) return badRequest(new InvalidParamError('email'));
         } catch (error) {
