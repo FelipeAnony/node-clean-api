1633e7ab087cfa4d3f3b9bffcfb8de5bd45a3a0c
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-03-04 17:06:54 +0100

    refactors: remove duplicated code

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -13,11 +13,13 @@ export class SignUpController implements Controller {
                 if (!body[field]) return badRequest(new MissingParamError(field));
             }
 
-            if (body.password !== body.passwordConfirmation) {
+            const { password, passwordConfirmation, email } = body;
+
+            if (password !== passwordConfirmation) {
                 return badRequest(new InvalidParamError('passwordConfirmation'));
             }
 
-            const emailIsValid = this.emailValidator.isValid(body.email);
+            const emailIsValid = this.emailValidator.isValid(email);
             if (!emailIsValid) return badRequest(new InvalidParamError('email'));
         } catch (error) {
             return internalServerError();
