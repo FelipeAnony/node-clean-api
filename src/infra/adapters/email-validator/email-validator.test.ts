9f28b0b185257b8ef0a4601116fbdc6408fc5a93
Felipe Mendonca <f.mendonca@cleverpy.com>
2023-03-07 20:44:23 +0100

    feat: ensures email-validator returns false if validator returns false

---
+++ src/infra/adapters/email-validator/email-validator.test.ts
@@ -0,0 +1,33 @@
+import { EmailValidator } from '@/infra/protocols';
+
+const makeSut = () => {
+    class ValidatorStub {
+        validate(email: string): boolean {
+            return true;
+        }
+    }
+
+    class EmailValidatorAdapter implements EmailValidator {
+        constructor(private readonly validator: any) {}
+
+        isValid(email: string): boolean {
+            return this.validator.validate(email);
+        }
+    }
+
+    const validatorStub = new ValidatorStub();
+    const sut = new EmailValidatorAdapter(validatorStub);
+
+    return { sut, validatorStub };
+};
+
+describe('EmailValidator Adapter', () => {
+    it('Should return false if validator returns false', () => {
+        const { sut, validatorStub } = makeSut();
+
+        jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(false);
+
+        const emailIsValid = sut.isValid('invalid-email');
+        expect(emailIsValid).toBe(false);
+    });
+});
